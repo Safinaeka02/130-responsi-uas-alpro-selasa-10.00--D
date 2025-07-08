68ba86ef923b13fd2ba042fe388a3c1b3de443db
Safinaeka02 <2400018130@webmail.uad.ac.id>
2025-07-08 13:14:59 +0700

    Add files via upload

---
+++ [130][RESPONSI UAS ALPRO][SELASA 10.00][BINTANG].cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<fstream>
+#include<vektor>
+#include<algoritm>
+
+using namespace std;
+
+struct pesawat(){
+	int nama;
+	int maskapai;
+	int rute;
+	int asal;
+	int tujuan;
+	int kode;
+	float jam keberangkatan;
+	float jam kedatangan;
+	double tanggal;
+	float harga;
+	
+}
+int main(){
+	cout <<"\n======================================================"; << endl;
+	cout <<"\nSELAMAT DATANG DI BANDARA SOEKARNO HATTA \n"; << endl;
+	cout <<"\n Oleh safina eka anindita \n"; << endl;
+	cout <<"\n======================================================" ;<< endl;
+	cout <<"Masukan nama penumpang :"; << endl;
+	cin >> nama;
+	cout <<"Masukkan nama maskapai :"; << endl;
+	cin >> maskapai;
+	cout <<"Maukkan bandara asal : " ;<<endl;465+4
+	cin >> asal;
+	cout <<"Masukkan bandara tujuan :"; <<endl;
+	cin >> tujuan;
+	cout <<"Masukkan kode :"; << endl;
+	cin >> kode;
+	cout <,"Masukkan jam keberangkatan :"; <<endl;
+	cin >> jamkeberangkatan;
+	cout <<"Masukkan jam kedatangan :"; << endl;
+	cin >> jamkedatangan;
+	cout <<"Masukkan tanggal :"; << endl;
+	cin >> tanggal;
+	cout <<"Masukkan harga :"; << endl;
+	cin >>harga;
+	cout << "\n======================================================="; << endl
+}
+
+void baca (){
+	ifstream <vektor> membacadata(){
+		bacadata(){
+			if(int i = 1; i < 6; i++){
+				if (int j = 1; j = 6; j++){
+					cout << bacadata;
+				}
+			}
+		} return baca;
+	}
+	
+void inputdatatiket (){
+	ofstream(){
+		if(file.txt)
+		operator oftream && file_data(){
+			cout << "Masukkan nama penumpang :"; << endl;
+			cin >> nama;
+			cout << "Masukkan nama maskapai :"; << endl;
+			cin >> maskapai;
+			cout <<"Masukkan rute :"; << endl;
+			cin >> rute;
+			cout << "Masukkan tanggal :"; <<endl;
+			cin >> tanggal;
+			cout <<"Masukkan jam :"; << endl;
+			cin >> jam;
+			cout <<"Masukkan harga :"; <<endl;
+			cin >> harga;
+	for(int i = 1; i < 6; i++){
+		for (int j = 1; j < 6; j++){
+			(!ditemukan)
+			cout <<"data yang anda masukkan tidak valid"; <<endl;
+		} else {
+			cout <<"file.txt";
+		}
+	} return true;
+	
+vektor tiketpesawat(){
+	cout <<"Menu cari berdasarkan ";
+	cout <<"1. mencari tiket berdasarkan kode " ;
+	cout <<"2. mencari maskapai berdasarkan nama";
+	cout <<"3. mencari berdasarkan tujuan";
+input data(){
+	
+}
+}
+			
+// implementasi booble sort mengurutkan dari harga yang terkecil ke yang terbesar
+void data(){
+	for(int i = 1; i < 6; i++){
+		for (int j = 1; j < 6; J++){
+			cout << terbesar;
+		}
+	} terurn false;
+}
+		}
+// implementasi shercing linear search mencari data berdasarkan nama
+void nama(){
+	
+	
+}
+	}
+}
